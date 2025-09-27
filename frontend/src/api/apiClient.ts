d0e42d817e30c9e4faf0885609cf3dc665a2bffd
satyam0777 <officialsatyam0777@gmail.com>
2025-09-27 18:15:21 +0530

    change the api in frontend and connect the backend

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,15 +1,14 @@
-
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: import.meta.env.VITE_API_URL as string, // tell TS it's a string
 });
 
 apiClient.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
